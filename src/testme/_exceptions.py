06e7f1669f8cdf9dfc483eaf23979b5e7c1fda1b
Kristoffer A. Wright <kawright@main.kriswrightdev.com>
2026-02-03 13:31:03 -0800

    doc exceptions

--- src/testme/_exceptions.py
+++ src/testme/_exceptions.py
@@ -9,13 +9,19 @@ import testme
 
 from typing import Optional
 
+#: Raised by ``testme.assert_*`` functions to signal the controlled failure of
+#: a test.
 class AssertFail(Exception):
 
+    #: Create a new instance of this class where assert_type indicates the type
+    #: of assertion that failed.
     def __init__(self, assert_type:testme.AssertType, reason:str) -> None:
         self._reason = reason
         super().__init__(reason)
         self._assert_type = assert_type
 
+    #: The string representation of this class includes is assertion type and
+    #: reason.
     def __repr__(self) -> str:
         return f"Failed ASSERT_{self.assert_type.value.upper()}: " \
             f"{self.reason}"
@@ -23,16 +29,20 @@ class AssertFail(Exception):
     def __str__(self) -> str:
         return self.__repr__()
 
+    #: The reason why this assertion failed.
     @property
     def reason(self) -> Optional[str]:
         return self._reason
 
+    #: The type of assertion that failed.
     @property
     def assert_type(self) -> testme.AssertType:
         return self._assert_type
 
+#: Raised by the user to force immediate termination of testing.
 class Abort(Exception):
    
+    #: Create a new instance of this class with an optional reason.
     def __init__(self, reason:Optional[str] = None) -> None:
         self._reason = reason
         if self._reason is None:
@@ -40,6 +50,7 @@ class Abort(Exception):
         else:
             super().__init__(reason)
 
+    #: The string representation of this class includes the reason.
     def __repr__(self) -> str:
         if self._reason is None:
             return ""
